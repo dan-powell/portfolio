7b7fca5303bf74517333beb0a313066f744bbe14
Dan Powell <dan@autumnrain.co.uk>
2015-08-13 00:20:18 +0100

    Improving asset controller

--- src/app/Http/Controllers/Api/AssetController.php
+++ src/app/Http/Controllers/Api/AssetController.php
@@ -14,83 +14,112 @@ use DanPowell\Portfolio\Models\Project;
 class AssetController extends Controller {
 
 
+    private $disk = 'portfolio';
+
+
     public function __construct()
     {
-        // Make sure only authorised users can Create Udate & Delete. 'Read' does not require authorisation.
-        $this->middleware('auth');
+        // Only authorised access
+        //$this->middleware('auth');
     }
 
 
     public function index(Request $request)
     {
-        if($path = $request->get('folder')){
 
-    	    $storage = Storage::disk('portfolio')->files($path);
-    	    if($storage){
-
-        	    $files = [];
-        	    foreach($storage as $file) {
-
-            	    $object = [];
-
-            	    $object['path'] = $file;
-            	    $object['extension'] = substr(strrchr($file, '.'), 1);
-            	    $object['filename'] = basename($file);
-                    $object['size'] = Storage::disk('portfolio')->size($file);
-                    $object['lastmodified'] = Storage::disk('portfolio')->lastModified($file);
-
-            	    $files[] = $object;
-        	    }
-
-                $folders = $this->getDirectories('portfolio', $path);
-
-                return response()->json(['files' => $files, 'folders' => $folders], 200);
-
-
-    	    } else {
-        	    dd('HONK');
-    	    }
-
+        if(!$path = $request->get('path')){;
+            $path = "";
         }
+
+        return response()->json([
+            'files' => $this->getFiles($this->disk, $path),
+            'folders' => $this->getDirectories($this->disk, $path)
+        ], 200);
+
     }
 
 
     public function store(Request $request)
     {
-        $file = $request->file('file');
 
-        $path = $request->get('path');
-        $name = $file->getClientOriginalName();
-		$type = $file->getClientMimeType();
+        $errors = [];
 
-		Storage::disk('portfolio')->put($path . '/' . $name,  File::get($file));
+        if (!$path = $request->get('path')) {
+            $errors[] = 'Path missing';
+        }
+
+
+        if (!$name = $request->get('name')) {
+            $errors[] = 'Name missing';
+        }
+
+
+        if(count($errors) > 0) {
+            return response()->json(['errors' => $errors], 422);
+        }
+
+        if ($file = $request->file('file')) {
+
+            $storage = Storage::disk($this->disk)->put($path . '/' . $name,  File::get($file));
+
+            if($storage) {
+                return response()->json(['file' => $path . '/' . $name], 200);
+            } else {
+                return response()->json(['errors' => $storage], 422);
+            }
+
+        } else {
+
+            $storage = Storage::disk($this->disk)->makeDirectory($path . '/' . $name);
+
+            if($storage) {
+                return response()->json(['folder' => $path . '/' . $name], 200);
+            } else {
+                return response()->json(['errors' => $storage], 422);
+            }
+
+        }
+
     }
 
 
-
-    protected function getDirectories($disk, $directory)
+    protected function getFiles($disk, $path)
     {
 
-        $folders = Storage::disk($disk)->directories($directory);
+        $files = Storage::disk($disk)->files($path);
 
-        $test = [];
-        if(count($folders) > 0){
+        $array = [];
+        foreach($files as $file) {
+    	    $object = [];
+    	    $object['path'] = $file;
+    	    $object['extension'] = substr(strrchr($file, '.'), 1);
+    	    $object['filename'] = basename($file);
+            $object['size'] = Storage::disk($disk)->size($file);
+            $object['lastmodified'] = Storage::disk($disk)->lastModified($file);
+
+    	    $array[] = $object;
+	    }
+
+	    return $array;
+    }
 
 
-            foreach($folders as $folder) {
-                $object = [];
+    protected function getDirectories($disk, $path)
+    {
 
-                $object['path'] = $folder;
-                $object['name'] =  substr(strrchr($folder, '/'), 1);
-                $object['folders'] = $this->getDirectories($disk, $folder);
+        $folders = Storage::disk($disk)->directories($path);
 
-                $test[] = $object;
+        $array = [];
+        foreach($folders as $folder) {
+            $object = [];
+            $object['path'] = $folder;
+            $object['name'] =  substr(strrchr($folder, '/'), 1);
+            $object['folders'] = $this->getDirectories($disk, $folder);
 
-            }
+            $array[] = $object;
         }
 
-        return $test;
-
+        return $array;
     }
 
 
